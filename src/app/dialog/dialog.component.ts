4d3619729f3cdb502fa6d77fb18aa7e68fb70497
Loew3nzahn <gather.daniel@yahoo.de>
2025-04-08 21:16:41 +0200

    added loading bar

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { User } from '../../models/user.class';
+import { Firestore, collection, doc, addDoc } from '@angular/fire/firestore';
+import { Injectable, OnInit } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NgIf } from '@angular/common';
+
 import {
   MatDialogActions,
   MatDialogClose,
@@ -26,16 +31,36 @@ import {
     MatFormFieldModule,
     MatIconModule,
     MatDatepickerModule,
+    MatProgressBarModule,
+    NgIf,
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
+  constructor() {}
+  loading: boolean = false;
+  firestore: Firestore = inject(Firestore);
   user = new User();
   birthDate: Date = new Date();
 
   saveUser() {
+    this.loading = true;
     this.user.birthDate = this.birthDate.getTime();
     console.log('user', this.user);
+
+    const userCollection = collection(this.firestore, 'users');
+    addDoc(userCollection, this.user.toJSON())
+      .then((result: any) => {
+        this.loading = false;
+        console.log(result);
+      })
+      .catch((error: any) => {
+        console.log('Ging nicht', error);
+      });
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
   }
 }
